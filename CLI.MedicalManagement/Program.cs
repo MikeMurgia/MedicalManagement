3459408ea821f8ea74a7811fa227d7e7a8576bad
mikem <mikem@MIKELAPTOP>
2025-09-22 20:29:59 -0400

    Add project files.

---
+++ CLI.MedicalManagement/Program.cs
@@ -0,0 +1,68 @@
+﻿using Library.MedicalManagement.Models;
+using System;
+
+
+namespace CLI.MedicalManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Medical Management!");
+            List<Patient?> patients = new List<Patient?>();
+
+            do
+            {
+                Console.WriteLine("1. Create Patient");
+                Console.WriteLine("2. Edit Patients");
+                Console.WriteLine("3. View Patients");
+                Console.WriteLine("4. Create Physicians");
+                Console.WriteLine("5. Edit Physicians");
+                Console.WriteLine("6. View Physicians");
+                Console.WriteLine("7. Create Appointment");
+                Console.WriteLine("8. View Appointment");
+                Console.WriteLine("9. Exit");
+
+                var userChoice = Console.ReadLine();
+
+                switch (userChoice)
+                {
+                    case "1":
+                        var name = Console.ReadLine() ?? string.Empty;
+                        var address = Console.ReadLine() ?? string.Empty;
+                        var birthdate = Console.ReadLine() ?? string.Empty;
+                        var race = Console.ReadLine() ?? string.Empty;
+                        var gender = Console.ReadLine() ?? string.Empty;
+                        var diagnoses = Console.ReadLine() ?? string.Empty;
+                        var prescriptions = Console.ReadLine() ?? string.Empty;
+                        int id = 1;
+                        Patient patient = new Patient(name, address, birthdate, race, gender, diagnoses, prescriptions, id);
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        foreach(var p in patients)
+                        {
+                            Console.WriteLine(p);
+                        }
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        break;
+                    case "7":
+                        break;
+                    case "8":
+                        break;
+                    case "9":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid command");
+                        break;
+                }
+            } while (true);
+        }
+    }
+}
---
+++ Library.MedicalManagement/Models/Patient.cs
@@ -0,0 +1,31 @@
+﻿namespace Library.MedicalManagement.Models
+{
+    public class Patient
+    {
+        private string? Name { get; set; }
+        private string? Address { get; set; }
+        private string? Birthdate { get; set; }
+        private string? Race { get; set; }
+        private string? Gender { get; set; }
+        private string? Diagnoses { get; set; }
+        private string? Prescriptions { get; set; }
+        public int Id { get; set; }
+
+        public Patient(string name, string address, string birthdate, string race, string gender, string diagnoses, string prescription, int id)
+        {
+            Name = name;
+            Address = address;
+            Birthdate = birthdate;
+            Race = race;
+            Gender = gender;
+            Diagnoses = diagnoses;
+            Prescriptions = prescription;
+            Id = id;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} | {Gender} | {Race} | DOB: {Birthdate} | {Address}";
+        }
+    }
+}
